37eeecc1003d3672b37241f082654a50b96fa183
albi.penayo <albi.penayo@gmail.com>
2021-08-20 18:20:26 -0300

    se agrego segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule,
