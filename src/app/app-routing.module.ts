3b3181fc43bb786a67fc8256f3620b40cc0c73f2
albi.penayo <albi.penayo@gmail.com>
2021-09-08 02:17:28 -0300

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Componentes/login/login.component';
+import { NoEncontradoComponent } from './Componentes/no-encontrado/no-encontrado.component';
+import { ValidadorGuard } from './helpers/validador.guard';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'formulario',component: PrimerComponenteComponent, canActivate:[ValidadorGuard]},
+  {path: '', component: LoginComponent},
+  {path: '**', component: NoEncontradoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
