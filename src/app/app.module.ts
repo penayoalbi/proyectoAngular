c0d1d17c796ec7650854c7f1b67d7b9d33729e28
albi.penayo <albi.penayo@gmail.com>
2021-09-08 02:14:25 -0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { MenuComponent } from './Componentes/menu/menu.component';
+import { NoEncontradoComponent } from './Componentes/no-encontrado/no-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimerComponenteComponent,
-    SegundoComponenteComponent
+    SegundoComponenteComponent,
+    LoginComponent,
+    MenuComponent,
+    NoEncontradoComponent
   ],
   imports: [
     BrowserModule,
